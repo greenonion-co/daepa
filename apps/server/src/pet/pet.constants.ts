001a200df59ca8e52a63daa779da8a8b33593cba
42KIM <ksahoo524@gmail.com>
2025-05-15 00:02:45 +0900

    PET_SPECIES에 종류 추가

--- apps/server/src/pet/pet.constants.ts
+++ apps/server/src/pet/pet.constants.ts
@@ -3,6 +3,12 @@ export const PET_SEX = {
   F: 'F',
   N: 'N',
 } as const;
+
 export const PET_SPECIES = {
-  CR: 'CR',
+  CR: 'CR', // 크레스티드게코
+  LE: 'LE', // 레오파드게코
+  PT: 'PT', // 펫테일게코
+  KN: 'KN', // 납테일게코
+  LC: 'LC', // 리키에너스
+  GG: 'GG', // 가고일게코
 } as const;
