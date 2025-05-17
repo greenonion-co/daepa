e3ec77c1e9d6ea93d048a6436f0560271401a0c7
42KIM <ksahoo524@gmail.com>
2025-05-17 17:29:09 +0900

    CORS 허용 옵션 추가

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -5,6 +5,10 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://localhost:4000'],
+    credentials: true,
+  });
 
   app.setGlobalPrefix('api');
 
