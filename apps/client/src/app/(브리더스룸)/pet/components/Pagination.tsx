240713d9d667cd64350fbea3a7bd818290884862
정예원 <yes3427@kookmin.ac.kr>
2025-05-20 11:53:34 +0900

    페이지네이션 기능 추가

--- apps/client/src/app/(브리더스룸)/pet/components/Pagination.tsx
+++ apps/client/src/app/(브리더스룸)/pet/components/Pagination.tsx
@@ -6,35 +6,47 @@ import { ChevronLeft } from "lucide-react";
 
 interface PaginationProps<TData> {
   table: Table<TData>;
+  pagination: {
+    page: number;
+    setPage: (page: number) => void;
+    totalPage?: number;
+    hasNextPage?: boolean;
+    hasPreviousPage?: boolean;
+  };
 }
 
-export function Pagination<TData>({ table }: PaginationProps<TData>) {
+export function Pagination<TData>({ table, pagination }: PaginationProps<TData>) {
   return (
-    <div className="flex items-center py-2">
+    <div className="flex items-center justify-between py-2">
       <div className="text-muted-foreground text-sm">
         <span className="font-bold text-blue-500">
           {table.getFilteredSelectedRowModel().rows.length}
         </span>{" "}
         / <span className="font-bold">{table.getFilteredRowModel().rows.length}</span>
       </div>
-      <div>
+      <div className="flex items-center">
         <Button
           variant="outline"
           size="sm"
-          onClick={() => table.previousPage()}
-          disabled={!table.getCanPreviousPage()}
+          onClick={() => pagination.setPage(pagination.page - 1)}
+          disabled={!pagination.hasPreviousPage}
         >
           <ChevronLeft />
         </Button>
+        <span className="mx-2">{pagination.page}</span>
+        <span>/</span>
+        <span className="mx-2 font-semibold">{pagination.totalPage}</span>
+
         <Button
           variant="outline"
           size="sm"
-          onClick={() => table.nextPage()}
-          disabled={!table.getCanNextPage()}
+          onClick={() => pagination.setPage(pagination.page + 1)}
+          disabled={!pagination.hasNextPage}
         >
           <ChevronRight />
         </Button>
       </div>
+      <div />
     </div>
   );
 }
