9c18ea9f2fdc1ba82e871defb28589d90a283b0f
42KIM <ksahoo524@gmail.com>
2025-05-25 15:40:45 +0900

    Parent 생성시 notification 자동 생성

--- apps/server/src/parent/parent.service.ts
+++ apps/server/src/parent/parent.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  forwardRef,
+  Inject,
+} from '@nestjs/common';
 import { ParentEntity } from './parent.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { In, Repository } from 'typeorm';
@@ -11,12 +16,18 @@ import {
 } from './parent.dto';
 import { instanceToPlain, plainToInstance } from 'class-transformer';
 import { PARENT_STATUS } from './parent.constant';
+import { PetService } from 'src/pet/pet.service';
+import { USER_NOTIFICATION_TYPE } from 'src/user_notification/user_notification.constant';
+import { UserNotificationService } from 'src/user_notification/user_notification.service';
 
 @Injectable()
 export class ParentService {
   constructor(
     @InjectRepository(ParentEntity)
     private readonly parentRepository: Repository<ParentEntity>,
+    @Inject(forwardRef(() => PetService))
+    private readonly petService: PetService,
+    private readonly userNotificationService: UserNotificationService,
   ) {}
 
   async findOne(petId: string, findParentDto: FindParentDto) {
@@ -41,12 +52,23 @@ export class ParentService {
   }
 
   async createParent(petId: string, createParentDto: CreateParentDto) {
-    // TODO: parent 성별 검증
-    return await this.parentRepository.insert({
+    await this.parentRepository.insert({
       pet_id: petId,
       parent_id: createParentDto.parentId,
       role: createParentDto.role,
+      is_my_pet: createParentDto.isMyPet ?? false,
+      status: createParentDto.isMyPet
+        ? PARENT_STATUS.APPROVED
+        : PARENT_STATUS.PENDING,
     });
+
+    if (!createParentDto.isMyPet) {
+      await this.createParentRequestNotification({
+        senderPetId: petId,
+        receiverPetId: createParentDto.parentId,
+        message: createParentDto.message,
+      });
+    }
   }
 
   async updateParentStatus(petId: string, updateParentDto: UpdateParentDto) {
@@ -68,6 +90,45 @@ export class ParentService {
       },
       {
         status: PARENT_STATUS.DELETED,
+      },
+    );
+  }
+
+  private async createParentRequestNotification({
+    senderPetId,
+    receiverPetId,
+    message,
+  }: {
+    senderPetId: string;
+    receiverPetId: string;
+    message?: string;
+  }) {
+    const senderPetSummary = await this.petService.getPetSummary(senderPetId);
+    if (!senderPetSummary) {
+      throw new NotFoundException(
+        'Sender pet(id: ' + senderPetId + ') not found',
+      );
+    }
+    const receiverPetSummary =
+      await this.petService.getPetSummary(receiverPetId);
+    if (!receiverPetSummary) {
+      throw new NotFoundException(
+        'Receiver pet(id: ' + receiverPetId + ') not found',
+      );
+    }
+
+    const notificationDetail = {
+      senderPet: senderPetSummary,
+      receiverPet: receiverPetSummary,
+      message,
+    };
+
+    await this.userNotificationService.createUserNotification(
+      senderPetSummary.ownerId,
+      {
+        receiverId: receiverPetSummary.ownerId,
+        type: USER_NOTIFICATION_TYPE.PARENT_REQUEST,
+        detailJson: notificationDetail,
       },
     );
   }
