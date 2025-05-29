457b501e8f62d6c04ecd85d19d1003f10a2b72d8
42KIM <ksahoo524@gmail.com>
2025-05-30 00:36:14 +0900

    MysqlError 타입 및 validator 공통화 추출

--- apps/server/src/egg/egg.controller.ts
+++ apps/server/src/egg/egg.controller.ts
@@ -8,26 +8,7 @@ import {
 import { EggService } from './egg.service';
 import { CreateEggDto } from './egg.dto';
 import { nanoid } from 'nanoid';
-
-class MySQLError extends Error {
-  code: string;
-
-  constructor(message: string, code: string) {
-    super(message);
-    this.code = code;
-  }
-}
-
-function isMySQLError(error: unknown): error is MySQLError {
-  return (
-    error instanceof MySQLError ||
-    (error instanceof Error &&
-      typeof error === 'object' &&
-      error !== null &&
-      'code' in error &&
-      typeof error.code === 'string')
-  );
-}
+import { isMySQLError } from 'src/common/error';
 
 @Controller('/v1/egg')
 export class EggController {
