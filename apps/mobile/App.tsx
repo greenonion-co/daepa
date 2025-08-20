4e322a08851374feba974dbd662b72f125514abd
정예원 <yes3427@kookmin.ac.kr>
2025-08-20 18:32:18 +0900

    Popup 공통 컴포넌트 제작

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { setupApiClient } from './src/utils/apiSetup';
 import Toast from '@/components/common/Toast';
 import Loading from '@/components/common/Loading';
+import Popup from '@/components/common/Popup';
 
 const queryClient = new QueryClient();
 
@@ -20,6 +21,7 @@ function App() {
         <NavigationContainer>
           <Toast ref={(ref: Toast | null) => Toast.setRef(ref)} />
           <Loading ref={(ref: Loading | null) => Loading.setRef(ref)} />
+          <Popup ref={(ref: Popup | null) => Popup.setRef(ref)} />
 
           <Navigation />
         </NavigationContainer>
