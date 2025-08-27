68da2a6ffbf6f85e7f3e5c9bfdbf8b9b45116d16
정예원 <yes3427@kookmin.ac.kr>
2025-08-27 11:15:58 +0900

    웹 403 인 경우 대응

--- packages/api-client/src/api/mutator/use-custom-instance.ts
+++ packages/api-client/src/api/mutator/use-custom-instance.ts
@@ -104,6 +104,13 @@ AXIOS_INSTANCE.interceptors.response.use(
       }
     }
 
+    if (error.response?.status === 403) {
+      if (typeof window !== "undefined") {
+        alert("권한이 없습니다. 관리자에게 문의해주세요.");
+        window.location.href = "/";
+      }
+    }
+
     return Promise.reject(error);
   },
 );
