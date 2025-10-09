a94508f524705b8595cdbf2687d340aca6bbe0d3
42KIM <ksahoo524@gmail.com>
2025-10-09 23:42:09 +0900

    RN 할일 주석 추가

--- apps/mobile/src/utils/apiSetup.ts
+++ apps/mobile/src/utils/apiSetup.ts
@@ -1,6 +1,8 @@
 import { setTokenProvider } from '@repo/api-client';
 import { useAuthStore } from '../store/auth';
 
+// TODO!: 환경에 따라 baseURL 설정
+
 export const setupApiClient = () => {
   const getToken = async () => useAuthStore.getState().accessToken ?? null;
   const setToken = async (token: string) =>
