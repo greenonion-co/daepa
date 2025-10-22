d6303f36a1f9afd8fb64b074bd61bd2e76fa8fc4
Yeewon <yes3427@kookmin.ac.kr>
2025-10-22 10:06:22 +0900

    메뉴바에 로그인 버튼 추가

--- apps/client/src/app/(브리더스룸)/components/Menubar.tsx
+++ apps/client/src/app/(브리더스룸)/components/Menubar.tsx
@@ -1,19 +1,23 @@
 import { SIDEBAR_ITEMS } from "../constants";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { Input } from "@/components/ui/input";
 import { Search } from "lucide-react";
 import { useSearchKeywordStore } from "../store/searchKeyword";
+import UserButton from "./UserButton";
 
 const Menubar = () => {
+  const router = useRouter();
   const pathname = usePathname();
   const { setSearchKeyword } = useSearchKeywordStore();
 
   return (
     <div className="flex h-[52px] items-center justify-between">
       <div className="flex items-center">
-        <div className="mr-10 font-bold">브리더스룸</div>
+        <div className="mr-10 cursor-pointer font-bold" onClick={() => router.push("/pet")}>
+          브리더스룸
+        </div>
         {SIDEBAR_ITEMS.map((item) => (
           <Link
             className={cn(
@@ -28,22 +32,26 @@ const Menubar = () => {
         ))}
       </div>
 
-      {pathname === "/pet" && (
-        <div className="flex items-center gap-2">
-          <div className="relative">
-            <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-gray-500" />
-            <Input
-              placeholder="펫 이름으로 검색하세요"
-              className="h-8 rounded-lg bg-gray-100 pl-9"
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                  setSearchKeyword(e.currentTarget.value);
-                }
-              }}
-            />
+      <div className="flex items-center gap-2">
+        {pathname === "/pet" && (
+          <div className="flex items-center gap-2">
+            <div className="relative">
+              <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-gray-500" />
+              <Input
+                placeholder="펫 이름으로 검색하세요"
+                className="h-8 rounded-lg bg-gray-100 pl-9"
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    setSearchKeyword(e.currentTarget.value);
+                  }
+                }}
+              />
+            </div>
           </div>
-        </div>
-      )}
+        )}
+
+        <UserButton />
+      </div>
     </div>
   );
 };
