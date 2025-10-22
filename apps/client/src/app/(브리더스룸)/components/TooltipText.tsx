0179b23a5e221e57900664936cbfb9abbdbdecb8
Yeewon <yes3427@kookmin.ac.kr>
2025-10-22 10:18:46 +0900

    TooltipText 콘텐츠 단위 단위 줄바꿈 적용

--- apps/client/src/app/(브리더스룸)/components/TooltipText.tsx
+++ apps/client/src/app/(브리더스룸)/components/TooltipText.tsx
@@ -23,11 +23,13 @@ const TooltipText = ({
           {text?.length > 10 ? `${text.slice(0, 10)}...` : text}
         </div>
       </TooltipTrigger>
-      <TooltipContent className="min-w-[200px] max-w-[400px] rounded-2xl border border-gray-300 bg-white p-5 font-[500] shadow-lg">
+      <TooltipContent className="min-w-[200px] max-w-[300px] rounded-2xl border border-gray-300 bg-white p-5 font-[500] shadow-lg">
         {title && <div className="text-[16px] font-[600] text-gray-800">{title}</div>}
         {description && <div className="pb-2 text-[12px] text-gray-500">{description}</div>}
 
-        <div className="text-[14px] text-gray-800">{content ?? text}</div>
+        <div className="whitespace-pre-wrap break-keep text-[14px] text-gray-800">
+          {content ?? text}
+        </div>
       </TooltipContent>
     </Tooltip>
   );
