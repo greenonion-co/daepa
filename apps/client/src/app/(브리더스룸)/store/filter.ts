63a71ffbeadc80ef48481a3b77f9db216d776d74
42KIM <ksahoo524@gmail.com>
2025-11-07 01:49:42 +0900

    사용되지 않는 column filters 옵션 제거

--- apps/client/src/app/(브리더스룸)/store/filter.ts
+++ apps/client/src/app/(브리더스룸)/store/filter.ts
@@ -1,22 +1,14 @@
 import { create } from "zustand";
-import { PetControllerFindAllParams, PetDto } from "@repo/api-client";
-import { VisibilityState } from "@tanstack/react-table";
+import { PetControllerFindAllParams } from "@repo/api-client";
 
 export interface FilterStore<PetControllerFindAllParams> {
   searchFilters: Partial<PetControllerFindAllParams>;
-  columnFilters?: VisibilityState;
   setSearchFilters: (filters: Partial<PetControllerFindAllParams>) => void;
-  setColumnFilters: (columnFilters: Partial<VisibilityState>) => void;
 }
 
 export const useFilterStore = create<FilterStore<PetControllerFindAllParams>>()((set) => ({
   searchFilters: {},
-  columnFilters: undefined,
 
   // Actions
   setSearchFilters: (filters) => set({ searchFilters: filters }),
-  setColumnFilters: (newFilter: Partial<Record<keyof PetDto, boolean>>) =>
-    set((state) => ({
-      columnFilters: state.columnFilters ? { ...state.columnFilters, ...newFilter } : newFilter,
-    })),
 }));
