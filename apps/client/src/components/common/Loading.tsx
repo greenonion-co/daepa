0757d5aeae5772a1af40fa9c95152e5f9c7863d3
Yeewon <yes3427@kookmin.ac.kr>
2026-01-08 23:46:48 +0900

    로딩 JSON을 정적 import로 변경

--- apps/client/src/components/common/Loading.tsx
+++ apps/client/src/components/common/Loading.tsx
@@ -1,22 +1,12 @@
 "use client";
 
 import Lottie from "lottie-react";
-import { useEffect, useState } from "react";
+import loadingAnimation from "@/../public/assets/animations/loading.json";
 
 const Loading = () => {
-  const [animationData, setAnimationData] = useState(null);
-
-  useEffect(() => {
-    fetch("/assets/animations/loading.json")
-      .then((response) => response.json())
-      .then((data) => setAnimationData(data));
-  }, []);
-
-  if (!animationData) return null;
-
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <Lottie animationData={animationData} loop style={{ width: 200, height: 200 }} />
+      <Lottie animationData={loadingAnimation} loop style={{ width: 200, height: 200 }} />
     </div>
   );
 };
