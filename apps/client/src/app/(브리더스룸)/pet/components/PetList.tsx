c315b0819e1c15172603a4148256c5553ad5e6da
42KIM <ksahoo524@gmail.com>
2026-02-04 00:24:50 +0900

    펫목록 필터 스크롤시 상단에 고정

--- apps/client/src/app/(브리더스룸)/pet/components/PetList.tsx
+++ apps/client/src/app/(브리더스룸)/pet/components/PetList.tsx
@@ -102,8 +102,8 @@ export default function PetList() {
   if (isLoading) return <Loading />;
 
   return (
-    <div className="min-h-screen space-y-1 bg-gray-100 pt-2 dark:bg-transparent">
-      <div className="px-2">
+    <div className="min-h-screen space-y-1 bg-gray-100 dark:bg-transparent">
+      <div className="sticky top-[52px] z-40 bg-gray-100 px-2 pt-2 pb-1 dark:bg-transparent">
         <Filters variant="light" />
       </div>
 
